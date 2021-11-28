9946b046110307aa83a6da4be78cc7ff7f79319e
srimannaini <srimannaini@gmail.com>
2021-11-28 18:57:06 +0100

    added files

---
+++ faceRecognizer/faceRecognizer-8python.py
@@ -0,0 +1,16 @@
+def RectCalc(w,l):
+    area=w*l
+    per=2*w+2*l
+    return area,per
+l1=3
+w1=5
+area1,per1=RectCalc(w1,l1)
+print('Rectangle area, per are:',area1,per1)
+l2=6
+w2=4
+area2,per2=RectCalc(w2,l2)
+print('Rectangle area, per are:',area2,per2)
+
+
+
+
---
+++ faceRecognizer/faceRecognizer-9-classes.py
@@ -0,0 +1,32 @@
+class Rectangle:
+    def __init__(self,c,w,l):    #method
+        self.width=w
+        self.length=l
+        self.color=c
+
+
+    def area(self):
+        self.area=self.width*self.length
+        return self.area
+
+    def per(self):
+        self.perimeter=2*self.width+2*self.length
+        return self.perimeter
+c1='red'
+w1=3
+l1=4
+rect1=Rectangle(c1,w1,l1)   #object of rectangle
+#print('Rectangle 1 is',rect1.color,'with area',rect1.area)
+areaRect1=rect1.area()
+print(areaRect1)
+per1=rect1.per()
+print('Rectangle 1 has perimeter:',per1)
+
+c2='blue'
+w2=3
+l2=8
+rect2=Rectangle(c2,w2,l2)   #object of rectangle
+#print('Rectangle 1 is',rect1.color,'with area',rect1.area)
+areaRect2=rect2.area()
+print(areaRect2)
+print('Rectangle 1 is:',rect1.color)
