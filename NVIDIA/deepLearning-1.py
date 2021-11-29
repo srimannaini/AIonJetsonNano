ea62c47f4077134c867e72ab07c44585c27af08d
srimannaini <srimannaini@gmail.com>
2021-11-29 10:24:37 +0100

    files added

--- NVIDIA/deepLearning-1.py
+++ NVIDIA/deepLearning-1.py
@@ -8,19 +8,19 @@ height=720
 #cam=jetson.utils.gstCamera(width,height,'/dev/video1')
 cam=jetson.utils.gstCamera(width,height,'0')
 net=jetson.inference.imageNet('googlenet')
-timeMark=time.time()
-fpsFilter=0
+timeMark=time.time()   #grab clock
+fpsFilter=0            #low pass filter
 timeMark=time.time()
 font=cv2.FONT_HERSHEY_SIMPLEX
 while True:
-    frame, width, height = cam.CaptureRGBA(zeroCopy=1)
-    classID, confidence = net.Classify(frame, width, height)
-    item = net.GetClassDesc(classID)
-    dt=time.time()-timeMark
-    fps=1/dt
-    fpsFilter=.95*fpsFilter+.05*fps
+    frame, width, height = cam.CaptureRGBA(zeroCopy=1)    #makle sure to give w & h
+    classID, confidence = net.Classify(frame, width, height)    #recognize image & gives confidence
+    item = net.GetClassDesc(classID)                            #Know the object
+    dt=time.time()-timeMark                                     #change in time 
+    fps=1/dt                                                    #calc fps
+    fpsFilter=.95*fpsFilter+.05*fps                             #
     timeMark=time.time()
-    frame=jetson.utils.cudaToNumpy(frame,width,height,4)
+    frame=jetson.utils.cudaToNumpy(frame,width,height,4)        #convert cuda2numpy
     frame=cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR).astype(np.uint8)
     cv2.putText(frame,str(round(fpsFilter,1))+'      '+item,(0,30),font,1,(0,0,255),2)
     cv2.imshow('webCam',frame)
@@ -29,3 +29,11 @@ while True:
         break
 cam.release()
 cv2.destroyAllWindows()
+
+
+
+
+'''
+fps=20
+no latency
+google net image recogn model
